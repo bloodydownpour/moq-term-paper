5681ff62022d3022a706c3281b76125b81d084b8
bloodydownpour <nikitosbarbos0308@gmail.com>
2025-04-19 13:48:34 +0300

    Added second unit test

---
+++ MoqUnitTests/ServiceUnitTests.cs
@@ -0,0 +1,38 @@
+﻿using Castle.Core.Logging;
+using Moq;
+using Xunit;
+
+namespace MoqTestingProject
+{
+    public class ServiceUnitTests
+    {
+        public ServiceUnitTests() { }   
+        [Fact]
+        public void GetPerson_ReturnsJohnDoe()
+        {
+            var mockRepo = new Mock<IPersonRepository>();
+            mockRepo.Setup(r => r.GetById(1)).Returns(new Person(1, "John", "Doe"));
+
+            var service = new PersonService(mockRepo.Object);
+
+            var result = service.GetById(1);
+            Assert.Equal(new Person(1, "John", "Doe").ToString(), result.ToString());
+        }
+
+
+        [Fact]
+        public void GetAllPersons_ReturnsIQueryableOfPersons()
+        {
+            var mockRepo = new Mock<IPersonRepository>();
+            var persons = new List<Person>
+            {
+                new Person(1, "John", "Doe"),
+                new Person(2, "Jane", "Smith"),
+                new Person(3, "Алёна", "Найдёнова")
+            }.AsQueryable();
+            mockRepo.Setup(repo => repo.GetAll()).Returns(persons);
+
+            var service = new PersonService(mockRepo.Object);
+        }
+    }
+}
