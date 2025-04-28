a69096c60a89c06a30f63655a02f8a1f99a4295d
Nikita <nikitosbarbos0308@gmail.com>
2025-04-28 10:16:03 +0300

    integration tests go brrrt

---
+++ MoqUnitTests/IntegrationTests.cs
@@ -0,0 +1,46 @@
+﻿using MoqTestingProject;
+using MoqTests.TestingSupplies;
+
+namespace MoqTests
+{
+    public class IntegrationTests : IClassFixture<DatabaseCleaner>
+    {
+        private readonly DatabaseCleaner _cleaner;
+
+        public IntegrationTests(DatabaseCleaner cleaner) => _cleaner = cleaner;
+
+        [Fact]
+        public async Task CanInsertPerson()
+        {
+            var person = new Person(1, "Test", "User");
+
+            _cleaner.Context.Persons.Add(person);
+            _cleaner.Context.SaveChanges();
+
+            var insertedUser = _cleaner.Context.Persons.First();
+            await _cleaner.CleanDatabase();
+
+            Assert.Equal("1: Test User", insertedUser.ToString());
+        }
+
+        [Fact]
+        public async Task CanUpdatePerson()
+        {
+            var person = new Person(2, "Test", "User");
+
+            _cleaner.Context.Persons.Add(person);
+            _cleaner.Context.SaveChanges();
+
+            person.Surname = "Updated User";
+
+            _cleaner.Context.SaveChanges();
+
+            var updatedUser = _cleaner.Context.Persons.First(); // заново берём из БД
+            await _cleaner.CleanDatabase();
+
+            Assert.Equal("2: Test Updated User", updatedUser.ToString());
+        }
+
+
+    }
+}
---
+++ MoqUnitTests/TestingSupplies/DatabaseCleaner.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoqTestingProject;
+
+namespace MoqTests.TestingSupplies
+{
+    public class DatabaseCleaner : IDisposable
+    {
+        public EFDbContext Context { get; private set; }
+
+        public DatabaseCleaner()
+        {
+            var options = new DbContextOptionsBuilder<EFDbContext>()
+           .UseInMemoryDatabase(Guid.NewGuid().ToString())
+           .Options;
+
+            Context = new EFDbContext(options);
+        }
+
+        public async Task CleanDatabase()
+        {
+            var persons = Context.Persons.ToList();
+            Context.Persons.RemoveRange(persons);
+            await Context.SaveChangesAsync();
+        }
+
+
+        public void Dispose()
+        {
+            Context.Database.EnsureDeleted();
+        }
+    }
+}
